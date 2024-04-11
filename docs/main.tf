aeb713d169b7f88493d21acef3690f095f62e437
Marta Jones <martajon10@gmail.com>
2024-04-11 12:42:48 +0200

    cambios

--- docs/main.tf
+++ docs/main.tf
@@ -1,27 +1,55 @@
 
-provider "docker" {
-  host = "tcp://localhost:2375"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
 }
 
+provider "docker" {}
+
+resource "docker_image" "docker_in_docker" {
+  name = "docker:dind"
+}
 
 resource "docker_container" "docker_in_docker" {
-  name  = "docker-in-docker"
-  image = "docker:dind"
+  name       = "docker-in-docker"
+  image      = "docker:dind"
   privileged = true
   ports {
     internal = 2375
     external = 2375
   }
+  volumes {
+    volume_name    = docker_volume.miVolumen.name
+    container_path = "/var/www/html"
+  }
 }
 
+resource "docker_image" "jenkins" {
+  name = "jenkins/jenkins:lts"
+}
 
 resource "docker_container" "jenkins" {
   name  = "jenkins"
-  image = "jenkins:jenkins-blueocean" 
+  image = "jenkins/jenkins:lts"
   ports {
-    internal = 8080
-    external = 8080
+    internal = 8081
+    external = 8081
   }
   depends_on = [docker_container.docker_in_docker]
+
+  volumes {
+    volume_name    = docker_volume.miVolumen.name
+    container_path = "/var/www/html"
+  }
+}
+
+
+//Creamos un recurso para el volumen de datos
+resource "docker_volume" "miVolumen" {
+  name = "mi-volumen-docker"
 }
 
